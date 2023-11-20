2ed6faf921bc6683503897bb4b9b40ae03209abd
Felipe Sousa <felipe.fps09@hotmail.com>
2023-11-20 18:06:23 -0300

    Create repository

--- src/main/java/com/felipesouls/client/repositories/ClientRepository.java
+++ src/main/java/com/felipesouls/client/repositories/ClientRepository.java
@@ -1,2 +1,7 @@
-package com.felipesouls.client.repositories;public interface ClientRepository {
+package com.felipesouls.client.repositories;
+
+import com.felipesouls.client.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
