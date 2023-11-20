90dc915534f1efa54ffbf16677c1c40f0c0fe100
Felipe Sousa <felipe.fps09@hotmail.com>
2023-11-20 18:05:51 -0300

    Create dto client

--- src/main/java/com/felipesouls/client/dto/ClientDTO.java
+++ src/main/java/com/felipesouls/client/dto/ClientDTO.java
@@ -1,2 +1,80 @@
-package com.felipesouls.client.dto;public class ClientDTO {
+package com.felipesouls.client.dto;
+
+import com.felipesouls.client.entities.Client;
+import org.springframework.beans.BeanUtils;
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientDTO() {
+    }
+
+    public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDTO(Client entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
 }
