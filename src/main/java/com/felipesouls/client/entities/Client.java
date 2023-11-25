3c02e596938cf2f46eb57d5eba19e326a4a20de2
Felipe Sousa <felipe.fps09@hotmail.com>
2023-11-25 10:48:12 -0300

    Included annotation @Column in variable cpf for unique value

--- src/main/java/com/felipesouls/client/entities/Client.java
+++ src/main/java/com/felipesouls/client/entities/Client.java
@@ -14,6 +14,8 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String cpf;
     private Double income;
     private LocalDate birthDate;
